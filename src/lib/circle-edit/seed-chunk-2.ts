701c4785802972b79ae1c1619e4cdef81c398459
firstaimrun73 <firstaimrun73@gmail.com>
2026-08-30 00:58:31 +0530

    Fix seed-chunk-2 with full PDF catalog data (fashion + vehicles)

--- src/lib/circle-edit/seed-chunk-2.ts
+++ src/lib/circle-edit/seed-chunk-2.ts
@@ -1,1 +1,101 @@
-PLACEHOLDER
+/** Seed TSV chunk 2 */
+export const SEED_CHUNK_2 = `sunglasses	Sunglasses	fashion	Fashion & Wearables	10	a realistic sunglasses
+reading-glasses	Reading Glasses	fashion	Fashion & Wearables	10	a realistic reading glasses
+baseball-cap	Baseball Cap	fashion	Fashion & Wearables	10	a realistic baseball cap
+beanie	Beanie	fashion	Fashion & Wearables	15	a realistic beanie
+fedora	Fedora	fashion	Fashion & Wearables	15	a realistic fedora
+cowboy-hat	Cowboy Hat	fashion	Fashion & Wearables	10	a realistic cowboy hat
+sun-hat	Sun Hat	fashion	Fashion & Wearables	10	a realistic sun hat
+helmet	Helmet	fashion	Fashion & Wearables	10	a realistic helmet
+crown	Crown	fashion	Fashion & Wearables	15	a realistic crown
+headband	Headband	fashion	Fashion & Wearables	20	a realistic headband
+hair-clip	Hair Clip	fashion	Fashion & Wearables	5	a realistic hair clip
+scrunchie	Scrunchie	fashion	Fashion & Wearables	5	a realistic scrunchie
+scarf	Scarf	fashion	Fashion & Wearables	10	a realistic scarf
+tie	Tie	fashion	Fashion & Wearables	10	a realistic tie
+bow-tie	Bow Tie	fashion	Fashion & Wearables	10	a realistic bow tie
+leather-belt	Leather Belt	fashion	Fashion & Wearables	10	a realistic leather belt
+watch	Watch	fashion	Fashion & Wearables	10	a realistic watch
+smartwatch	Smartwatch	fashion	Fashion & Wearables	10	a realistic smartwatch
+bracelet	Bracelet	fashion	Fashion & Wearables	5	a realistic bracelet
+necklace	Necklace	fashion	Fashion & Wearables	10	a realistic necklace
+pendant-necklace	Pendant Necklace	fashion	Fashion & Wearables	15	a realistic pendant necklace
+earrings	Earrings	fashion	Fashion & Wearables	20	a realistic earrings
+ring	Ring	fashion	Fashion & Wearables	20	a realistic ring
+brooch	Brooch	fashion	Fashion & Wearables	20	a realistic brooch
+handbag	Handbag	fashion	Fashion & Wearables	20	a realistic handbag
+backpack	Backpack	fashion	Fashion & Wearables	25	a realistic backpack
+tote-bag	Tote Bag	fashion	Fashion & Wearables	10	a realistic tote bag
+clutch-bag	Clutch Bag	fashion	Fashion & Wearables	5	a realistic clutch bag
+wallet	Wallet	fashion	Fashion & Wearables	5	a realistic wallet
+sneakers	Sneakers	fashion	Fashion & Wearables	10	a realistic sneakers
+running-shoes	Running Shoes	fashion	Fashion & Wearables	5	a realistic running shoes
+boots	Boots	fashion	Fashion & Wearables	5	a realistic boots
+heels	Heels	fashion	Fashion & Wearables	5	a realistic heels
+sandals	Sandals	fashion	Fashion & Wearables	5	a realistic sandals
+slippers	Slippers	fashion	Fashion & Wearables	10	a realistic slippers
+denim-jacket	Denim Jacket	fashion	Fashion & Wearables	15	a realistic denim jacket
+leather-jacket	Leather Jacket	fashion	Fashion & Wearables	10	a realistic leather jacket
+blazer	Blazer	fashion	Fashion & Wearables	20	a realistic blazer
+hoodie	Hoodie	fashion	Fashion & Wearables	10	a realistic hoodie
+sweater	Sweater	fashion	Fashion & Wearables	10	a realistic sweater
+t-shirt	T-Shirt	fashion	Fashion & Wearables	15	a realistic t-shirt
+dress	Dress	fashion	Fashion & Wearables	15	a realistic dress
+skirt	Skirt	fashion	Fashion & Wearables	25	a realistic skirt
+jeans	Jeans	fashion	Fashion & Wearables	15	a realistic jeans
+cargo-pants	Cargo Pants	fashion	Fashion & Wearables	10	a realistic cargo pants
+shorts	Shorts	fashion	Fashion & Wearables	15	a realistic shorts
+suit	Suit	fashion	Fashion & Wearables	15	a realistic suit
+leather-gloves	Leather Gloves	fashion	Fashion & Wearables	20	a realistic leather gloves
+umbrella	Umbrella	fashion	Fashion & Wearables	15	a realistic umbrella
+raincoat	Raincoat	fashion	Fashion & Wearables	15	a realistic raincoat
+sedan	Sedan	vehicles	Vehicles & Transport	30	a realistic sedan
+sports-car	Sports Car	vehicles	Vehicles & Transport	30	a realistic sports car
+supercar	Supercar	vehicles	Vehicles & Transport	30	a realistic supercar
+classic-car	Classic Car	vehicles	Vehicles & Transport	35	a realistic classic car
+electric-car	Electric Car	vehicles	Vehicles & Transport	35	a realistic electric car
+suv	SUV	vehicles	Vehicles & Transport	30	a realistic suv
+pickup-truck	Pickup Truck	vehicles	Vehicles & Transport	30	a realistic pickup truck
+jeep	Jeep	vehicles	Vehicles & Transport	30	a realistic jeep
+motorcycle	Motorcycle	vehicles	Vehicles & Transport	35	a realistic motorcycle
+scooter	Scooter	vehicles	Vehicles & Transport	20	a realistic scooter
+bicycle	Bicycle	vehicles	Vehicles & Transport	25	a realistic bicycle
+mountain-bike	Mountain Bike	vehicles	Vehicles & Transport	25	a realistic mountain bike
+road-bike	Road Bike	vehicles	Vehicles & Transport	30	a realistic road bike
+electric-bike	Electric Bike	vehicles	Vehicles & Transport	30	a realistic electric bike
+skateboard	Skateboard	vehicles	Vehicles & Transport	30	a realistic skateboard
+roller-skates	Roller Skates	vehicles	Vehicles & Transport	30	a realistic roller skates
+bus	Bus	vehicles	Vehicles & Transport	30	a realistic bus
+school-bus	School Bus	vehicles	Vehicles & Transport	30	a realistic school bus
+city-bus	City Bus	vehicles	Vehicles & Transport	25	a realistic city bus
+train	Train	vehicles	Vehicles & Transport	30	a realistic train
+subway-train	Subway Train	vehicles	Vehicles & Transport	35	a realistic subway train
+tram	Tram	vehicles	Vehicles & Transport	20	a realistic tram
+trolley	Trolley	vehicles	Vehicles & Transport	20	a realistic trolley
+freight-train	Freight Train	vehicles	Vehicles & Transport	20	a realistic freight train
+locomotive	Locomotive	vehicles	Vehicles & Transport	20	a realistic locomotive
+taxi	Taxi	vehicles	Vehicles & Transport	25	a realistic taxi
+police-car	Police Car	vehicles	Vehicles & Transport	30	a realistic police car
+ambulance	Ambulance	vehicles	Vehicles & Transport	25	a realistic ambulance
+fire-truck	Fire Truck	vehicles	Vehicles & Transport	25	a realistic fire truck
+delivery-van	Delivery Van	vehicles	Vehicles & Transport	30	a realistic delivery van
+camper-van	Camper Van	vehicles	Vehicles & Transport	25	a realistic camper van
+rv-motorhome	RV Motorhome	vehicles	Vehicles & Transport	25	a realistic rv motorhome
+tractor	Tractor	vehicles	Vehicles & Transport	25	a realistic tractor
+forklift	Forklift	vehicles	Vehicles & Transport	25	a realistic forklift
+excavator	Excavator	vehicles	Vehicles & Transport	30	a realistic excavator
+bulldozer	Bulldozer	vehicles	Vehicles & Transport	35	a realistic bulldozer
+crane-truck	Crane Truck	vehicles	Vehicles & Transport	30	a realistic crane truck
+cement-mixer	Cement Mixer	vehicles	Vehicles & Transport	20	a realistic cement mixer
+sailboat	Sailboat	vehicles	Vehicles & Transport	30	a realistic sailboat
+yacht	Yacht	vehicles	Vehicles & Transport	30	a realistic yacht
+speedboat	Speedboat	vehicles	Vehicles & Transport	35	a realistic speedboat
+canoe	Canoe	vehicles	Vehicles & Transport	35	a realistic canoe
+kayak	Kayak	vehicles	Vehicles & Transport	25	a realistic kayak
+ferry-boat	Ferry Boat	vehicles	Vehicles & Transport	35	a realistic ferry boat
+cruise-ship	Cruise Ship	vehicles	Vehicles & Transport	30	a realistic cruise ship
+submarine	Submarine	vehicles	Vehicles & Transport	35	a realistic submarine
+helicopter	Helicopter	vehicles	Vehicles & Transport	35	a realistic helicopter
+private-jet	Private Jet	vehicles	Vehicles & Transport	20	a realistic private jet
+passenger-airplane	Passenger Airplane	vehicles	Vehicles & Transport	35	a realistic passenger airplane
+hot-air-balloon	Hot Air Balloon	vehicles	Vehicles & Transport	35	a realistic hot air balloon`;
