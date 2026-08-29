ae6b327f5b855c1fde40d03018ca9055291b9868
firstaimrun73 <firstaimrun73@gmail.com>
2026-08-30 01:00:35 +0530

    Overwrite seed-chunk-3 and seed-chunk-4 with complete 100-asset PDF data each

--- src/lib/circle-edit/seed-chunk-4.ts
+++ src/lib/circle-edit/seed-chunk-4.ts
@@ -1,1 +1,53 @@
-PLACEHOLDER
+/** Seed TSV chunk 4 */
+export const SEED_CHUNK_4 = `paintbrush	Paintbrush	art	Art & Creator Props	10	a realistic paintbrush
+paint-palette	Paint Palette	art	Art & Creator Props	10	a realistic paint palette
+canvas	Canvas	art	Art & Creator Props	10	a realistic canvas
+easel	Easel	art	Art & Creator Props	15	a realistic easel
+sketchbook	Sketchbook	art	Art & Creator Props	10	a realistic sketchbook
+pencil	Pencil	art	Art & Creator Props	5	a realistic pencil
+pen	Pen	art	Art & Creator Props	5	a realistic pen
+marker	Marker	art	Art & Creator Props	5	a realistic marker
+crayon	Crayon	art	Art & Creator Props	5	a realistic crayon
+chalk	Chalk	art	Art & Creator Props	5	a realistic chalk
+clay	Clay	art	Art & Creator Props	10	a realistic clay
+sculpture	Sculpture	art	Art & Creator Props	20	a realistic sculpture
+camera	Camera	art	Art & Creator Props	15	a realistic camera
+film-camera	Film Camera	art	Art & Creator Props	20	a realistic film camera
+tripod	Tripod	art	Art & Creator Props	10	a realistic tripod
+microphone	Microphone	art	Art & Creator Props	15	a realistic microphone
+headphones	Headphones	art	Art & Creator Props	10	a realistic headphones
+speaker	Speaker	art	Art & Creator Props	15	a realistic speaker
+guitar	Guitar	art	Art & Creator Props	20	a realistic guitar
+piano	Piano	art	Art & Creator Props	25	a realistic piano
+violin	Violin	art	Art & Creator Props	20	a realistic violin
+drum	Drum	art	Art & Creator Props	15	a realistic drum
+suitcase	Suitcase	travel	Travel & Lifestyle	15	a realistic suitcase
+backpack-travel	Travel Backpack	travel	Travel & Lifestyle	15	a realistic travel backpack
+passport	Passport	travel	Travel & Lifestyle	10	a realistic passport
+map	Map	travel	Travel & Lifestyle	5	a realistic map
+compass	Compass	travel	Travel & Lifestyle	10	a realistic compass
+tent	Tent	travel	Travel & Lifestyle	20	a realistic tent
+sleeping-bag	Sleeping Bag	travel	Travel & Lifestyle	15	a realistic sleeping bag
+camping-stove	Camping Stove	travel	Travel & Lifestyle	15	a realistic camping stove
+lantern	Lantern	travel	Travel & Lifestyle	10	a realistic lantern
+binoculars	Binoculars	travel	Travel & Lifestyle	15	a realistic binoculars
+water-bottle	Water Bottle	travel	Travel & Lifestyle	5	a realistic water bottle
+thermos	Thermos	travel	Travel & Lifestyle	10	a realistic thermos
+coffee-mug	Coffee Mug	travel	Travel & Lifestyle	5	a realistic coffee mug
+tea-cup	Tea Cup	travel	Travel & Lifestyle	5	a realistic tea cup
+wine-glass	Wine Glass	travel	Travel & Lifestyle	10	a realistic wine glass
+champagne-flute	Champagne Flute	travel	Travel & Lifestyle	10	a realistic champagne flute
+plate	Plate	travel	Travel & Lifestyle	5	a realistic plate
+bowl	Bowl	travel	Travel & Lifestyle	5	a realistic bowl
+fork	Fork	travel	Travel & Lifestyle	5	a realistic fork
+knife	Knife	travel	Travel & Lifestyle	5	a realistic knife
+spoon	Spoon	travel	Travel & Lifestyle	5	a realistic spoon
+chopsticks	Chopsticks	travel	Travel & Lifestyle	5	a realistic chopsticks
+candle	Candle	travel	Travel & Lifestyle	5	a realistic candle
+vase	Vase	travel	Travel & Lifestyle	10	a realistic vase
+book	Book	travel	Travel & Lifestyle	5	a realistic book
+magazine	Magazine	travel	Travel & Lifestyle	5	a realistic magazine
+newspaper	Newspaper	travel	Travel & Lifestyle	5	a realistic newspaper
+eyeglasses	Eyeglasses	travel	Travel & Lifestyle	10	a realistic eyeglasses
+toothbrush	Toothbrush	travel	Travel & Lifestyle	15	a realistic toothbrush
+razor	Razor	travel	Travel & Lifestyle	15	a realistic razor`;
