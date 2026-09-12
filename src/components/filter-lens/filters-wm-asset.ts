e8dda8d5742004fdb4fe17aa84b3068e8bf3f2e3
firstaimrun73 <firstaimrun73@gmail.com>
2026-09-12 17:51:04 +0530

    chore(filters): remove broken watermark PNG assembler (unused)

--- src/components/filter-lens/filters-wm-asset.ts
+++
@@ -1,9 +0,0 @@
-/** Exact Motio2edit Filters watermark PNG (supplied asset pixels). Do not replace with CSS/text. */
-import { P0 } from "./filters-wm-p0";
-import { P1 } from "./filters-wm-p1";
-import { P2 } from "./filters-wm-p2";
-import { P3 } from "./filters-wm-p3";
-import { P4 } from "./filters-wm-p4";
-import { P5 } from "./filters-wm-p5";
-import { P6 } from "./filters-wm-p6";
-export const FILTERS_WM_DATA_URL = "data:image/png;base64," + P0 + P1 + P2 + P3 + P4 + P5 + P6;
